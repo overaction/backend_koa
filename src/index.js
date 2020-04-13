3f91f6827261b2f88f3d3940dcb618c7b367bc17
overaction <kmc04@naver.com>
2020-04-13 14:34:14 +0000

    라우트 모듈화

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
 //라우터 설정
-router.get('/', ctx => {
-    ctx.body = '홈';
-});
-router.get('/about/:nm?', ctx => {
-    const {nm} = ctx.params;
-    //name의 존재 유무에 따라 다른 결과 출력
-    ctx.body = nm ? `${nm}의 소개` : '소개';
-});
-
-router.get('/posts',ctx => {
-    const {idd} = ctx.query;
-    //id의 존재 유무에 따라 다른 결과 출력
-    ctx.body = idd ? `포스트 #${idd}` : '포스트 아이디가 없습니다.';
-})
+router.use('/api',api.routes()); //api 라우트 적용
+// '/api' 경로가 메인 경로, '/api/test'등으로 적용 가능
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
