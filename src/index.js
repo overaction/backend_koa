3bcae3a14c6ebb0babe0e06023bc72c60e080fff
overaction <kmc04@naver.com>
2020-04-13 14:05:24 +0000

    라우트 파라미터와 쿼리

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
-    if(ctx.query.authorized !== '1') {
-        ctx.status = 401;
-        return;
-    }
-    await next();
-    console.log('END');
+//라우터 설정
+router.get('/', ctx => {
+    ctx.body = '홈';
+});
+router.get('/about/:nm?', ctx => {
+    const {nm} = ctx.params;
+    //name의 존재 유무에 따라 다른 결과 출력
+    ctx.body = nm ? `${nm}의 소개` : '소개';
 });
 
-app.use((ctx,next) => {
-    console.log(2);
-    next();
-});
+router.get('/posts',ctx => {
+    const {idd} = ctx.query;
+    //id의 존재 유무에 따라 다른 결과 출력
+    ctx.body = idd ? `포스트 #${idd}` : '포스트 아이디가 없습니다.';
+})
 
-app.use(ctx => {
-    ctx.body = 'hello world';
-});
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
